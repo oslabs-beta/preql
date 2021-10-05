c397e02fb51d621a45c4711ab9c803eaf60d60df
Connor Tracy <connortracy15@gmail.com>
2021-10-05 11:11:19 -0400

    webpack working. stylesheet loaded

---
+++ src/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div>Home</div>
+      <div>About</div>
+    </div>
+  );
+}
+
+export default Navbar
